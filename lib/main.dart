f4f0468c5bc710e2be56cb0e46e0481e91653fc7
johnpalaios <johnpalaios123@gmail.com>
2023-01-19 21:23:50 +0200

    Initialized flutter project and added map

---
+++ lib/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_map/flutter_map.dart'; // Suitable for most situations
+import 'package:flutter_map/plugin_api.dart'; // Only import if required functionality is not exposed by default
+import 'package:latlong2/latlong.dart';
+
+void main() {
+  runApp(
+    MaterialApp(home: MyApp()), // use MaterialApp
+  );
+}
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return FlutterMap(
+    options: MapOptions(
+        center: LatLng(51.509364, -0.128928),
+        zoom: 9.2,
+    ),
+    children: [
+        TileLayer(
+            urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
+            userAgentPackageName: 'com.example.app',
+        ),
+    ],
+); 
+  }
+}
